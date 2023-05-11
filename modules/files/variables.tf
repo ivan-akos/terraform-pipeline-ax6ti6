87059efe9b22d61f989d10121b18f22fd2102eab
ivan-akos <114164620+ivan-akos@users.noreply.github.com>
2023-05-11 12:32:42 +0200

    a

---
+++ modules/files/variables.tf
@@ -0,0 +1,15 @@
+variable "number_of_files" {
+  description = "How many files to create"
+  type        = number
+  default     = 3
+}
+
+variable "content" {
+  description = ""
+  type        = string
+}
+
+variable "name" {
+  description = "The name of the files"
+  type        = string
+}
